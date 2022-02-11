7867f99c800e9b1914f2b046b7d5ba57c7111ba3
Muhammad Maaz Ahmed <maazahmed78624@gmail.com>
2022-02-11 13:57:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const body = document.querySelector("body");
+const party = document.querySelectorAll(".party");
+const votes = document.querySelectorAll(".vote");
+const btn = document.getElementById("btn");
+
+body.addEventListener("keypress", (event) => {
+  votes.forEach((vote) => {
+        let voteCounter = parseInt(vote.textContent);
+        voteCounter++;
+        if (
+          (event.key == 1 && vote.classList.contains("vote1")) ||
+          (event.key == 2 && vote.classList.contains("vote2")) ||
+          (event.key == 3 && vote.classList.contains("vote3")) ||
+          (event.key == 4 && vote.classList.contains("vote4"))
+        ) {
+            let age = Number(prompt("enter age :"))
+            if(age >= 18 && age <= 79){
+                vote.textContent = voteCounter;
+            }else if(age < 18){
+                alert("you are underage")
+            }else if(age > 79){
+                alert("you are overage")
+            }
+        }
+  });
+});
+
+
